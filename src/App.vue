e3bdcd4fa02076ab91ee2526831cb9577644c5a1
정종찬 <jeongjongchan@jeongjongchan-ui-MacBookAir.local>
2022-03-08 21:21:48 +0900

    [0308] 리팩토링 리스트 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-header></todo-header>
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
+    <todo-list
+      v-bind:propsdata="todoItems"
+      v-on:removeItem="removeOneItem(todoItem, index)"
+    ></todo-list>
+    <todo-footer v-on:clearTodoAll="clearTodo"></todo-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
+  data: function () {
+    return {
+      todoItems: [],
+    };
+  },
+  methods: {
+    addOneItem: function (todoItem) {
+      let obj = { completed: false, item: todoItem };
+      localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    clearTodo: function () {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeOneItem: function (todoItem, index) {
+      this.todoItems.splice(index, 1);
+    },
+  },
+  created: function () {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(
+          JSON.parse(localStorage.getItem(localStorage.key(i)))
+          // this.todoItems.push(localStorage.key(i));
+        );
+      }
+    }
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    TodoHeader,
+    TodoFooter,
+    TodoInput,
+    TodoList,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f6f6f6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <ul>
+      <!-- 반복문 -->
+      <li
+        class="shadow"
+        v-for="(todoItem, index) in propsdata"
+        v-bind:key="todoItem.item"
+      >
+        <i
+          class="checkBtn"
+          v-on:click="toggleComplete(todoItem, index)"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          >체크버튼</i
+        >
+        <span v-bind:class="{ textCompleted: todoItem.completed }">
+          {{ todoItem.item }}
+        </span>
+
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i> -- </i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  methods: {
+    removeTodo: function (todoItem, index) {
+      this.$emit("removeItem", todoItem, index);
+    },
+    toggleComplete: function (todoItem) {
+      todoItem.completed = !todoItem.completed;
+      // 로컬 스토리지의 데이터를 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
